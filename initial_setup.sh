7f7a391508cd91b7965cec0aa6d6cb07856a10ac
Tim <maildokursu@gmail.com>
2021-10-18 15:52:55 +0200

    'sshkeys problem'

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,7 +3,7 @@
 # Create new (sudo) user, setup firewall, manage SSH keys
 
 # Initial commands
-# apt update -y && apt upgrade -y && apt install -y git
+# apt update -y && apt upgrade -y && apt install -y git curl
 # git clone https://github.com/dondontim/setup.git && cd setup
 
 
@@ -100,7 +100,7 @@ echo "--> Done"
 
 echo "Copy and paste below command on your local machine"
 echo "This will create your ssh private and public key and copy to clipboard the public key:"
-echo "ssh-keygen > /dev/null && cat ~/.ssh/id_rsa.pub | pbcopy && echo 'You have copied the public key to clipboard'"
+echo "ssh-keygen && cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard'"
 echo ""
 echo "If you execute above command then copy and paste it here using CTRL + V and press Enter"
 
@@ -120,79 +120,6 @@ chown -R "$user_to_create":"$user_to_create" "${new_user_home}/.ssh"
 
 
 
-
-
-
-sshd_config='/etc/ssh/sshd_config'
-
-echo "--> Copying ${sshd_config} to ${sshd_config}.bak"
-cp "${sshd_config}" "${sshd_config}.bak"
-echo "--> Done"
-
-
-echo "--> Disabling Password Authentication"
-
-echo "PasswordAuthentication no" >> "$sshd_config"
-echo "PermitEmptyPasswords no"   >> "$sshd_config"
-echo "PermitRootLogin no"        >> "$sshd_config" # PermitRootLogin without-password
-
-
-# Use more cryptographicaly secure protocol
-#echo "Protocol 2" >> "$sshd_config"
-# To test if SSH protocol 1 is supported any more, run the command:
-# ssh -1 user@remote-IP
-#
-# ssh -2 user@remote-IP # for Protocol 2
-
-
-# Set SSH Connection Timeout Idle Value
-#echo "ClientAliveInterval 600" >> "$sshd_config"
-
-# Total number of checkalive message sent by the ssh server 
-# without getting any response from the ssh client
-#echo "ClientAliveCountMax 0" >> "$sshd_config"
-
-# Limit SSH Access to Certain Users
-#echo "AllowUsers ${user_to_create}" >> "$sshd_config" # after space add other users
-# AllowGroups sysadmin dba
-
-# Configure a Limit for Password Attempts
-#echo "MaxAuthTries 3" >> "$sshd_config"
-
-# Allow 20 sec to login, if not disconnect
-#echo "LoginGraceTime 20" >> "$sshd_config"
-
-# Set non default port
-#echo "Port 7822" >> "$sshd_config"
-
-
-# TODO(tim): More about hardening sshd_config:
-# https://www.digitalocean.com/community/tutorials/how-to-harden-openssh-on-ubuntu-18-04
-
-
-
-
-
-
-
-# Restart ssh
 echo ""
-echo "--> Restarting ssh"
-sudo systemctl restart ssh # sshd on other distros than debian/ubuntu
-
-
-
-remote_machine_public_ip=$(curl https://ipecho.net/plain; echo)
-echo ""
-echo "Run below command on local machine to setup SSH keys and upload them here"
-echo "After this creat passphrase and pass passowrd for the user you created"
-echo ""
-echo "ssh-keygen && ssh-copy-id ${user_to_create}@${remote_machine_public_ip} -p 7822"
-
-
-
-echo ""
-echo "--> Finished!"
-echo "Now you should logout and connect again now using SSH keys"
-
-
+echo "logout and login once again with ssh key"
+echo "Finished!"
