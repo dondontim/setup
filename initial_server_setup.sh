071d4535b8eb3fb1f6b684873bd56dc2bd09f6e1
Tim <maildokursu@gmail.com>
2021-10-20 22:27:57 +0200

    'handled path with gnu coreutils for macos'

--- initial_server_setup.sh
+++ initial_server_setup.sh
@@ -4,6 +4,59 @@
 
 # Ref: https://www.digitalocean.com/community/tutorials/automating-initial-server-setup-with-ubuntu-18-04
 
+
+
+
+
+
+function command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+function install_nonexisting_command() {
+  if command_exists "$1"; then
+    echo "$1 exists"
+  else
+    sudo apt-get install -y "$1"
+  fi
+}
+
+
+function press_anything_to_continue() {
+  read -n 1 -s -r -p "Press any key to continue"
+  # -n defines the required character count to stop reading
+  # -s hides the user's input
+  # -r causes the string to be interpreted "raw" (without considering backslash escapes)
+  echo ""
+}
+
+
+function apt_install() {
+  sudo apt-get install -y "$@"
+}
+
+function update_and_upgrade() {
+  sudo apt-get update -y && sudo apt-get upgrade -y
+}
+
+
+
+
+
+
+update_and_upgrade
+apt_install ufw
+
+# Disable welcome message - https://askubuntu.com/a/676381 
+if [ -d "etc/update-motd.d" ]; then 
+  chmod -x /etc/update-motd.d/* 
+fi;
+
+
+
+
+
+
 set -euo pipefail
 
 ########################
@@ -19,11 +72,11 @@ COPY_AUTHORIZED_KEYS_FROM_ROOT=false
 
 # Additional public keys to add to the new sudo user
 # OTHER_PUBLIC_KEYS_TO_ADD=(
-#     "ssh-rsa AAAAB..."
-#     "ssh-rsa AAAAB..."
+#   "ssh-rsa AAAAB..."
+#   "ssh-rsa AAAAB..."
 # )
 OTHER_PUBLIC_KEYS_TO_ADD=(
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+YLvFLyTnbrcBIRIU5OMf1ZHkxM04jP9L/gnQlEKn0kd2XGYkBd36QRN51M9nnmIjaVTORLmv2DA0CsMoQRdmL7TphMXeycA6xi82wrf5cvy9Qbtp1iv0VfZR2YfXmXJnyT7+LY99Qf4qbPmnFpu384HR17IdeXmKvQf8tCqJBzph/pBYFLjZQxR/lWFYnvZ3e7Tb+7/IKaacLcusZfc9lQ1i+SXQ/kEuscHjI4rOjmzWqY/VfFFV6mSAmXKIQxuypY/wWXYlUuYFfEug41sRWCROPT7f3NwVumiOwB1G+FCTP6YS9KMFvhzfLqdC0igZ5rHkkVtUFAcAdh349FdpGeaR3C/7KwN/d+rUCJ9rKPBnOTrFbCOmTA44wwCRGoPLTIrmIfXpZqg51+ZpEiZSdS/tNfmc+N74g3Phq7NBx5GWJRa4S51D1xpsFS+Ba5Yj/q9cBAPwBKFHe+m1hcl1f2bS3NRm9zaRTiLcnTT4hptexhanhj8PtWzhSVAz4a8= dontim@android-571979f94aa0e9ac"
+  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+YLvFLyTnbrcBIRIU5OMf1ZHkxM04jP9L/gnQlEKn0kd2XGYkBd36QRN51M9nnmIjaVTORLmv2DA0CsMoQRdmL7TphMXeycA6xi82wrf5cvy9Qbtp1iv0VfZR2YfXmXJnyT7+LY99Qf4qbPmnFpu384HR17IdeXmKvQf8tCqJBzph/pBYFLjZQxR/lWFYnvZ3e7Tb+7/IKaacLcusZfc9lQ1i+SXQ/kEuscHjI4rOjmzWqY/VfFFV6mSAmXKIQxuypY/wWXYlUuYFfEug41sRWCROPT7f3NwVumiOwB1G+FCTP6YS9KMFvhzfLqdC0igZ5rHkkVtUFAcAdh349FdpGeaR3C/7KwN/d+rUCJ9rKPBnOTrFbCOmTA44wwCRGoPLTIrmIfXpZqg51+ZpEiZSdS/tNfmc+N74g3Phq7NBx5GWJRa4S51D1xpsFS+Ba5Yj/q9cBAPwBKFHe+m1hcl1f2bS3NRm9zaRTiLcnTT4hptexhanhj8PtWzhSVAz4a8= dontim@android-571979f94aa0e9ac"
 )
 
 CUSTOM_SSH_PORT=7822
@@ -39,14 +92,14 @@ useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
 encrypted_root_pw="$(grep root /etc/shadow | cut --delimiter=: --fields=2)"
 
 if [ "${encrypted_root_pw}" != "*" ]; then
-    # Transfer auto-generated root password to user if present
-    # and lock the root account to password-based access
-    echo "${USERNAME}:${encrypted_root_pw}" | chpasswd --encrypted
-    passwd --lock root
+  # Transfer auto-generated root password to user if present
+  # and lock the root account to password-based access
+  echo "${USERNAME}:${encrypted_root_pw}" | chpasswd --encrypted
+  passwd --lock root
 else
-    # Delete invalid password for user if using keys so that a new password
-    # can be set without providing a previous value
-    passwd --delete "${USERNAME}"
+  # Delete invalid password for user if using keys so that a new password
+  # can be set without providing a previous value
+  passwd --delete "${USERNAME}"
 fi
 
 # Expire the sudo user's password immediately to force a change
@@ -58,12 +111,12 @@ mkdir --parents "${home_directory}/.ssh"
 
 # Copy `authorized_keys` file from root if requested
 if [ "${COPY_AUTHORIZED_KEYS_FROM_ROOT}" = true ]; then
-    cp /root/.ssh/authorized_keys "${home_directory}/.ssh"
+  cp /root/.ssh/authorized_keys "${home_directory}/.ssh"
 fi
 
 # Add additional provided public keys
 for pub_key in "${OTHER_PUBLIC_KEYS_TO_ADD[@]}"; do
-    echo "${pub_key}" >> "${home_directory}/.ssh/authorized_keys"
+  echo "${pub_key}" >> "${home_directory}/.ssh/authorized_keys"
 done
 
 # Adjust SSH configuration ownership and permissions
@@ -74,7 +127,11 @@ chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
 # Disable root SSH login with password
 sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
 if sshd -t -q; then
-    systemctl restart sshd
+  # How to restart ssh
+  # https://www.cyberciti.biz/faq/how-do-i-restart-sshd-daemon-on-linux-or-unix/
+  # TODO(tim): in original here was sshd. Find out why sshd or ssh
+  systemctl restart ssh
+  systemctl restart sshd
 fi
 
 # Add exception for SSH and then enable UFW firewall
