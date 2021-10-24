8aa0828c7282bd5c163dc312fb049fcfd3467a9a
Tim <maildokursu@gmail.com>
2021-10-24 02:35:23 +0200

    'managin replacements in sshd_config'

--- initial_server_setup.sh
+++ initial_server_setup.sh
@@ -46,6 +46,36 @@ sshd_config='/etc/ssh/sshd_config'
 file_templates_dir="${PWD}/z_file_templates"
 
 
+
+
+
+
+function replace_regex_in_file() 
+{ 
+  local pattern repl flags file
+  pattern="$1"
+  repl="$2"
+  file="$3"
+  #sed --in-place -E "s/${pattern}/${repl}/" "$3" # it was macos sed
+  sed --in-place -E "s/${pattern}/${repl}/m" "$3"
+  # -i == --in-place
+}
+
+function edit_sshd_config() 
+{ 
+  # TODO(tim): delete it cuz at top of script you have globaly set with correct path
+  local sshd_config
+  sshd_config="$HOME/setup/sshd_config"
+  # Edit ssh config - $sshd_config
+  replace_regex_in_file "$@" "$sshd_config"
+}
+
+
+
+
+
+
+
 update_and_upgrade
 apt_install ufw
 
@@ -81,10 +111,10 @@ OTHER_PUBLIC_KEYS_TO_ADD=(
   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+YLvFLyTnbrcBIRIU5OMf1ZHkxM04jP9L/gnQlEKn0kd2XGYkBd36QRN51M9nnmIjaVTORLmv2DA0CsMoQRdmL7TphMXeycA6xi82wrf5cvy9Qbtp1iv0VfZR2YfXmXJnyT7+LY99Qf4qbPmnFpu384HR17IdeXmKvQf8tCqJBzph/pBYFLjZQxR/lWFYnvZ3e7Tb+7/IKaacLcusZfc9lQ1i+SXQ/kEuscHjI4rOjmzWqY/VfFFV6mSAmXKIQxuypY/wWXYlUuYFfEug41sRWCROPT7f3NwVumiOwB1G+FCTP6YS9KMFvhzfLqdC0igZ5rHkkVtUFAcAdh349FdpGeaR3C/7KwN/d+rUCJ9rKPBnOTrFbCOmTA44wwCRGoPLTIrmIfXpZqg51+ZpEiZSdS/tNfmc+N74g3Phq7NBx5GWJRa4S51D1xpsFS+Ba5Yj/q9cBAPwBKFHe+m1hcl1f2bS3NRm9zaRTiLcnTT4hptexhanhj8PtWzhSVAz4a8= dontim@android-571979f94aa0e9ac"
 )
 
-#CUSTOM_SSH_PORT=7822
+CUSTOM_SSH_PORT=7822
 
 PORTS_TO_BE_OPEN=(
-  "22"
+  "OpenSSH" # Add exception for SSH default port 22
   "21"
   "25"
   "7822"
@@ -104,7 +134,9 @@ usermod -aG sudo "${USERNAME}"
 
 
 
-
+################################################################################
+# Set up SSH keys                                                              #
+################################################################################
 
 # Create SSH directory for sudo user
 home_directory="$(eval echo ~${USERNAME})"
@@ -125,8 +157,100 @@ chmod 0700 "${home_directory}/.ssh"
 chmod 0600 "${home_directory}/.ssh/authorized_keys"
 chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
 
-# Disable root SSH login with password
-sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+
+
+################################################################################
+# sshd_config
+################################################################################
+
+# TODO(tim): after tests if this is enough
+# Original was this: ^PermitRootLogin.*
+
+
+
+echo "--> Copying ${sshd_config} to ${sshd_config}.bak"
+cp "${sshd_config}" "${sshd_config}.bak"
+echo "--> Done"
+
+
+# If variable not null change server SSH port
+if [ -n "$CUSTOM_SSH_PORT" ]; then
+  # Set non default port
+  edit_sshd_config "^Port.*$" "Port ${CUSTOM_SSH_PORT}" # change: 'Port N'
+  edit_sshd_config "GatewayPort.*$" "GatewayPort ${CUSTOM_SSH_PORT}" # change: 'GatewayPort N'
+fi
+
+
+
+### PasswordAuthentication - Disable password authentication for all users
+edit_sshd_config "^#PasswordAuthentication.*$" "PasswordAuthentication no"
+
+### PermitEmptyPasswords - 
+# When password authentication is allowed, it specifies whether the server 
+# allows login to accounts with empty password strings. The default is no.
+edit_sshd_config "^#PermitEmptyPasswords.*$" "PermitEmptyPasswords no"
+
+### PermitRootLogin
+edit_sshd_config "^PermitRootLogin.*$" "PermitRootLogin no" 
+# alternative: PermitRootLogin without-password or prohibit-password
+
+
+### ClientAliveInterval
+# Set SSH Connection Timeout Idle Value
+edit_sshd_config "^#ClientAliveInterval.*$" "ClientAliveInterval 600"
+
+
+### ClientAliveCountMax
+# Total number of checkalive message sent by the ssh server 
+# without getting any response from the ssh client
+edit_sshd_config "^#ClientAliveCountMax.*$" "ClientAliveCountMax 0"
+
+
+### MaxAuthTries
+# Configure a Limit for Password Attempts
+edit_sshd_config "^#MaxAuthTries.*$" "MaxAuthTries 3"
+
+### LoginGraceTime
+# Allow 20 sec to login, if not disconnect
+edit_sshd_config "^#LoginGraceTime.*$" "LoginGraceTime 20"
+
+
+
+### Protocol 2
+# Use more cryptographicaly secure protocol
+echo "Protocol 2" >> "$sshd_config"
+# To test if SSH protocol 1 is supported any more, run the command:
+# ssh -1 user@remote-IP
+#
+# ssh -2 user@remote-IP # for Protocol 2
+
+
+### AllowUsers
+# Limit SSH Access to Certain Users
+#echo "AllowUsers user1 user2" >> "$sshd_config" # after space add other users
+# AllowGroups sysadmin dba
+
+
+
+# TODO(tim): More about hardening sshd_config:
+# https://www.digitalocean.com/community/tutorials/how-to-harden-openssh-on-ubuntu-18-04
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if sshd -t -q; then
   # How to restart ssh
   # https://www.cyberciti.biz/faq/how-do-i-restart-sshd-daemon-on-linux-or-unix/
@@ -144,8 +268,16 @@ if sshd -t -q; then
   systemctl restart sshd
 fi
 
+
+
+
+
+################################################################################
+# Setting Up a Basic Firewall                                                  #
+################################################################################
+
 # Add exception for SSH and then enable UFW firewall
-ufw allow OpenSSH
+#ufw allow OpenSSH
 
 # If isset
 #if [ -n "$CUSTOM_SSH_PORT" ]; then
@@ -157,6 +289,7 @@ for port in "${PORTS_TO_BE_OPEN[@]}"; do
   ufw allow "$port"
 done
 
+# start/enable UFW firewall
 ufw --force enable
 
 
@@ -169,3 +302,23 @@ echo "ssh-keygen && ssh-copy-id ${user_to_create}@${remote_machine_public_ip} -p
 
 
 # TODO(tim): add here check if the ~/.ssh/authorized_keys exist
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
