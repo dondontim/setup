e391ef398bc734b14f4e168279f1430ae1660b57
Tim <maildokursu@gmail.com>
2021-11-07 23:34:31 +0100

    fixed typo

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -7,7 +7,7 @@ echo "Linked dotfiles. Please restart your shell. "
 
 # Create symlinks to all dotfiles from and ~/
 for f in "${user_to_setup_for_home_directory}/setup/dotfiles/\.[^.]*"; do
-  FILE="$(basename $f)"
+  FILE="$(basename "$f")"
   ln -sf "${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}" "$user_to_setup_for_home_directory"
   # No need cuz whole setup dir is users ownership
   #chown -R "${user_to_setup_for}:${user_to_setup_for}" "${user_to_setup_for_home_directory}/${FILE}"
