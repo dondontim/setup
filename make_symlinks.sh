0f122874ebf11ef066db5d53631a1926507f07f4
Tim <maildokursu@gmail.com>
2021-11-08 01:21:49 +0100

    some fixed stuff

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -6,13 +6,16 @@
 
 # Create symlinks to all dotfiles from and ~/
 for f in "${user_to_setup_for_home_directory}/setup/dotfiles/*"; do
-  [ -e "$f" ] || continue
+  [ -e "$f" ] && continue
   FILE="$(basename "$f")"
-  if ln -sf "${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}" "$user_to_setup_for_home_directory/${FILE}"; then
-    echo "(+) Linked: ${user_to_setup_for_home_directory}/setup/dotfiles/${FILE} to -> ${user_to_setup_for_home_directory}/${FILE}"
-  else
-    echo "(-) Failed linking: ${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}"
-  fi
+
+  ln -sf "${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}" "$user_to_setup_for_home_directory/${FILE}"
+
+  #if ln -sf "${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}" "$user_to_setup_for_home_directory/${FILE}"; then
+  #  echo "(+) Linked: ${user_to_setup_for_home_directory}/setup/dotfiles/${FILE} to -> ${user_to_setup_for_home_directory}/${FILE}"
+  #else
+  #  echo "(-) Failed linking: ${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}"
+  #fi
   
   # No need cuz whole setup dir is users ownership
   #chown -R "${user_to_setup_for}:${user_to_setup_for}" "${user_to_setup_for_home_directory}/${FILE}"
