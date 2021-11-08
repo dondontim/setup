b9b12d958c594893f18c642578ff4e7b1e61b0b9
Tim <maildokursu@gmail.com>
2021-11-08 01:40:03 +0100

    some fixed stuff

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -5,10 +5,10 @@
 
 
 # Create symlinks to all dotfiles from and ~/
-for f in "${user_to_setup_for_home_directory}/setup/dotfiles/*"; do
+for f in "${user_to_setup_for_home_directory}"/setup/dotfiles/.*; do
   #[ -e "$f" ] && continue
   FILE="$(basename "$f")"
-  
+
   echo "${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}"
   ln -sf "${user_to_setup_for_home_directory}/setup/dotfiles/${FILE}" "$user_to_setup_for_home_directory"
 
